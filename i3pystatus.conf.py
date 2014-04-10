1eb73ad18370d5667e7c2a8fdeed637917d2d521
sharkdp <davidpeter@web.de>
2014-04-10 14:14:26 +0200

    braces to v+b, move capslock->esc to xorg.conf

---
+++ i3pystatus.conf.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+from i3pystatus import Status
+
+status = Status(standalone=True)
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+    format="%a %-d %b %X",)
+
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp",
+    format="{temp:.0f}°C",)
+
+status.register("cpu_usage",
+    format="{usage:02}%",)
+# The battery monitor has many formatting options, see README for details
+
+# This would look like this, when discharging (or charging)
+# ↓14.22W 56.15% [77.81%] 2h:41m
+# And like this if full:
+# =14.22W 100.0% [91.21%]
+#
+# This would also display a desktop notification (via dbus) if the percentage
+# goes below 5 percent while discharging. The block will also color RED.
+status.register("battery",
+    format="{status} {consumption:.0f}W {percentage_design:.0f}% {remaining:%E%hh:%Mm}",
+    alert=True,
+    path="/sys/class/power_supply/BAT1/uevent",
+    alert_percentage=10,
+    status={
+        "DIS": "↓",
+        "CHR": "↑",
+        "FULL": "=",
+    },)
+
+# This would look like this:
+# Discharging 6h:51m
+# status.register("battery",
+#     format="{status} {remaining:%E%hh:%Mm}",
+#     alert=True,
+#     alert_percentage=5,
+#     status={
+#         "DIS":  "Discharging",
+#         "CHR":  "Charging",
+#         "FULL": "Bat full",
+#     },)
+
+# Shows the address and up/down state of eth0. If it is up the address is shown in
+# green (the default value of color_up) and the CIDR-address is shown
+# (i.e. 10.10.10.42/24).
+# If it's down just the interface name (eth0) will be displayed in red
+# (defaults of format_down and color_down)
+#
+# Note: the network module requires PyPI package netifaces-py3
+status.register("network",
+    interface="enp8s0",
+    format_up="{v4cidr}",)
+
+# Has all the options of the normal network and adds some wireless specific things
+# like quality and network names.
+#
+# Note: requires both netifaces-py3 and basiciw
+status.register("wireless",
+    interface="wlp9s0",
+    format_up="{essid} {quality:03.0f}%",)
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+status.register("disk",
+    path="/",
+    format="{avail}G",)
+
+status.run()
+
