f566546aecc6d285575ba7d845b55972a472f5d7
sharkdp <davidpeter@web.de>
2014-10-28 16:30:49 +0100

    added bootstrappig script

---
+++ minimal-config/bootstrap.sh
@@ -0,0 +1,15 @@
+set -e
+
+rootpath="https://raw.githubusercontent.com/sharkdp/config-files/master/minimal-config"
+
+files=.vimrc .bashrc .alias
+
+bd="$HOME/.backup-config-files"
+
+mkdir "${bd}$"
+for file in $files; do
+    path="$HOME/$file"
+    echo "Backing up and downloading new $file"
+    cp -v "$path" "${bd}/$file"
+    wget -O "$path" "${rootpath}/$file"
+done
