a06be3646fe1ccba7d369686031089f230bdcff0
sharkdp <davidpeter@web.de>
2014-10-28 16:35:28 +0100

    fixed bootstrap.sh; added README

---
+++ minimal-config/README.md
@@ -0,0 +1,5 @@
+Install via `wget`:
+
+```sh
+wget --no-check-certificate -O - https://raw.githubusercontent.com/sharkdp/config-files/master/minimal-config/bootstrap.sh | bash
+```
--- minimal-config/bootstrap.sh
+++ minimal-config/bootstrap.sh
@@ -2,14 +2,14 @@ set -e
 
 rootpath="https://raw.githubusercontent.com/sharkdp/config-files/master/minimal-config"
 
-files=.vimrc .bashrc .alias
+files=".vimrc .bashrc .alias"
 
 bd="$HOME/.backup-config-files"
+mkdir "${bd}"
 
-mkdir "${bd}$"
 for file in $files; do
     path="$HOME/$file"
     echo "Backing up and downloading new $file"
     cp -v "$path" "${bd}/$file"
-    wget -O "$path" "${rootpath}/$file"
+    wget -q --no-check-certificate -O "$path" "${rootpath}/$file"
 done
