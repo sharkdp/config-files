91e1c1e14bb74816de74ae21eeb6ae6c2097bce6
sharkdp <davidpeter@web.de>
2014-11-02 00:05:41 +0100

    install zsh autocompletion

--- shell/install.sh
+++ shell/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Install Oh My Zsh!
 if ! [[ -e ~/.oh-my-zsh ]]; then
      git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+# Install Zsh Syntax Highlighting
+if ! [[ -e ~/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting ]]; then
+    mkdir ~/.oh-my-zsh-custom/plugins
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting
 fi
