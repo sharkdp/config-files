43bd83f3c63bf64549f0b52bb86201ea0124eaac
sharkdp <davidpeter@web.de>
2014-11-01 22:15:22 +0100

    fixed tex environment, updated oh-my-zsh configuration, new custom folder

--- shell/install.sh
+++ shell/install.sh
@@ -3,7 +3,3 @@
 if ! [[ -e ~/.oh-my-zsh ]]; then
      git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
-
-# Install the custom files
-mkdir -p ~/.oh-my-zsh/custom/themes
-ln -sf "$(pwd)/shell/oh-my-zsh/custom/themes/shark.zsh-theme" ~/.oh-my-zsh/custom/themes/shark.zsh-theme
