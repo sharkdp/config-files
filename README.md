784335d5f54e7c20cc167413de25bbdc59632324
sharkdp <davidpeter@web.de>
2014-11-01 14:26:57 +0100

    install script oh my zsh

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+Before installing, make sure the following packages are installed:
+```
+git vim zsh
+```
 To install, clone the repo and run the `bootstrap` script
 ```bash
 git clone https://github.com/sharkdp/config-files.git ~/.cf
--- shell/install.sh
+++ shell/install.sh
@@ -1,4 +1,8 @@
-# TODO: install oh my zsh
+#!/bin/bash
+
+if ! [[ -e ~/.oh-my-zsh ]]; then
+     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
 
 # Install the custom files
 mkdir -p ~/.oh-my-zsh/custom/themes
