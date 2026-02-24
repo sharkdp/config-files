66ee233b1a4efef4a6a2901204a799207ed865b7
David Peter <mail@david-peter.de>
2026-02-24 10:08:40 +0100

    remove unused options

--- nvim/init.lua
+++ nvim/init.lua
@@ -209,15 +209,6 @@ require("lazy").setup({
 
       { -- Adds git related signs to the gutter, as well as utilities for managing changes
         'lewis6991/gitsigns.nvim',
-        opts = {
-          signs = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
-          },
-        },
         config = function()
           require('gitsigns').setup({
             -- Your existing gitsigns configuration
